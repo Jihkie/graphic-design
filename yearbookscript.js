0cd75487dcefeca27e2f73b202b645bea04fbae9
Jihkie <18361844+Jihkie@users.noreply.github.com>
2018-04-24 09:25:11 -0700

    Update yearbookscript.js

--- yearbookscript.js
+++ yearbookscript.js
@@ -1,8 +1,8 @@
 $(document).ready(() => {
   $('.link').click(function(){
     var panelName = $(this).attr('info');
-    $('.panel').hide(0, function(){
-      $('.'+panelName).show();
+    $('.panel').hide(500, function(){
+      $('.'+panelName).show(500);
     });
   });
   
